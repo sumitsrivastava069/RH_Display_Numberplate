23ce1c1b6419d4b33f6653c9e2ca583d0e6e4ffe
sumitsrivastava069 <61084331+sumitsrivastava069@users.noreply.github.com>
2023-06-08 17:09:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def render_image():
     return render_template('images/latest_image.html', image_data=image_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000
+    app.run(host='0.0.0.0', port=5000)
