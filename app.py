4c88d8b18ff49916ca59477ff1e95f39719d5df6
sumitsrivastava069 <61084331+sumitsrivastava069@users.noreply.github.com>
2023-06-10 13:44:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,35 +3,47 @@ import csv
 import os
 
 app = Flask(__name__, static_folder='static')
+latest_image_data = {}
 
-@app.route('/')
-def render_image():
-    image_data = []
+def update_image_data():
+    global latest_image_data
 
     # Get the current directory
     current_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Read the CSV file
     csv_file_path = os.path.join(current_dir, 'Frames', 'output.csv')
+    latest_entry = {}
+
     with open(csv_file_path, 'r') as file:
         csv_reader = csv.reader(file)
-        next(csv_reader)  # Skip header row
+        header = next(csv_reader)  # Skip header row
         for row in csv_reader:
             image_filename = row[0]
-            date_time = row[1]
-            number_plate = row[2]
+            number_plate = row[3]
+            date_time = row[2]
+            color = row[4]
 
-            image_path = os.path.join(image_filename)
+            image_path = os.path.join('static', image_filename)
 
-            print('image' - image_path)  # Print the image path
+            if color == 'Green' or color == 'Red':
+                entry = {
+                    'image_path': image_path,
+                    'date_time': date_time,
+                    'number_plate': number_plate,
+                    'color': color
+                }
 
-            image_data.append({
-                'image_path': image_path,
-                'date_time': date_time,
-                'number_plate': number_plate
-            })
+                if not latest_entry or date_time > latest_entry['date_time']:
+                    latest_entry = entry
+        print(image_path)
+    latest_image_data = latest_entry
 
-    return render_template('images/index.html', image_data=image_data)
+@app.route('/')
+def render_image():
+    return render_template('images/index.html', image_data=latest_image_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    update_image_data()  # Initial data update
+
+    app.run(host='0.0.0.0', port=5000,debug=True)
